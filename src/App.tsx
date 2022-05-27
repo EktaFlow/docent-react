50202daa0646e2bc553ba50c80b2212bdfaee1ad
John Yoo <jyoo300@gmail.com>
2022-05-26 19:58:13 -0700

    Added file to question

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,7 @@ const App: React.FC = () => {
           />
           <Route
             exact
-            path="/action-tiem"
+            path="/action-item"
             render={(props) => {
               return isAuthed() ? <ActionItems /> : <Login />;
             }}
--- src/app/Framework/Header.tsx
+++ src/app/Framework/Header.tsx
@@ -1,6 +1,6 @@
 import './Framework.scss';
 import { IonButton, IonPopover, IonContent } from '@ionic/react';
-import {logoutUser} from '../../api/api';
+import { logoutUser } from '../../api/api';
 
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -27,7 +27,7 @@ const Header: React.FC<{ showAssessment?: boolean, assessmentId?: number }> = ({
     })
   }
 
-  async function logout(){
+  async function logout() {
     var logout = await logoutUser();
     if (logout.data) {
       localStorage.removeItem("token");
@@ -51,9 +51,9 @@ const Header: React.FC<{ showAssessment?: boolean, assessmentId?: number }> = ({
         <IonButton expand='full' color="light" routerLink="/home">Home</IonButton>
 
         {showAssessment && <IonButton id="" expand='full' color="light" onClick={navigateToAssessment}>Continue Assessment</IonButton>}
-        {/* {showAssessment && <IonButton id="trigger-reports" expand='full' color="light">Reports</IonButton>} */}
-        {showAssessment && <IonButton expand='full' color="light" onClick={() => showReportsPopover(!reportsPopover)}>Reports</IonButton>}
 
+        {/* reports popover */}
+        {showAssessment && <IonButton id="trigger-reports" expand='full' color="light">Reports</IonButton>}
         <IonPopover trigger="trigger-reports">
           <IonButton color="light" expand="full" onClick={() => navigateToReports("mrl-summary")}>MRL Summary</IonButton>
           <IonButton color="light" expand="full" onClick={() => navigateToReports("questions-list")}>Questions List</IonButton>
@@ -63,7 +63,9 @@ const Header: React.FC<{ showAssessment?: boolean, assessmentId?: number }> = ({
           <IonButton color="light" expand="full" onClick={() => navigateToReports("risk-summary")}>MRL Risk Summary</IonButton>
           <IonButton color="light" expand="full" onClick={() => navigateToReports("detailed-risk")}>Detailed Risk Report</IonButton>
         </IonPopover>
-        <IonPopover
+
+        {/* {showAssessment && <IonButton expand='full' color="light" onClick={() => showReportsPopover(!reportsPopover)}>Reports</IonButton>} */}
+        {/* <IonPopover
           className="reports-popover"
           isOpen={reportsPopover}
           onDidDismiss={() => showReportsPopover(!reportsPopover)}
@@ -76,9 +78,21 @@ const Header: React.FC<{ showAssessment?: boolean, assessmentId?: number }> = ({
           <IonButton color="light" expand="full" onClick={() => navigateToReports("risk-summary")}>MRL Risk Summary</IonButton>
           <IonButton color="light" expand="full" onClick={() => navigateToReports("detailed-risk")}>Detailed Risk Report</IonButton>
         </IonPopover>
+        */}
 
-        <IonButton expand='full' color="light" onClick={() => showHelpPopover(!helpPopover)}>Help</IonButton>
+        {/* help popover */}
 
+        <IonButton id="trigger-help" expand='full' color="light">Help</IonButton>
+        <IonPopover trigger="trigger-help">
+          <IonButton color="light" expand="full" routerLink="criteria">Criteria</IonButton>
+          <IonButton color="light" expand="full" routerLink="definitions">Definitions</IonButton>
+          <IonButton color="light" expand="full" routerLink="acronyms">Acronyms</IonButton>
+          <IonButton color="light" expand="full" routerLink="faqs">FAQs</IonButton>
+          <IonButton color="light" expand="full">User's Guide</IonButton>
+          <IonButton color="light" expand="full" href="http://www.dodmrl.com/MRL%20Deskbook%20V2020.pdf">MRL Deskbook</IonButton>
+        </IonPopover>
+
+        {/* <IonButton expand='full' color="light" onClick={() => showHelpPopover(!helpPopover)}>Help</IonButton> 
         <IonPopover
           className="help-popover"
           isOpen={helpPopover}
@@ -90,7 +104,7 @@ const Header: React.FC<{ showAssessment?: boolean, assessmentId?: number }> = ({
           <IonButton color="light" expand="full" routerLink="faqs">FAQs</IonButton>
           <IonButton color="light" expand="full">User's Guide</IonButton>
           <IonButton color="light" expand="full" href="http://www.dodmrl.com/MRL%20Deskbook%20V2020.pdf">MRL Deskbook</IonButton>
-        </IonPopover>
+        </IonPopover> */}
 
         <IonButton expand='full' color="light" onClick={logout}>Logout</IonButton>
       </div>
