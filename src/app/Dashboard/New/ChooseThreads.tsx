6615705173e96daf253a4c73dbb856f71f5bc5ff
Ekta Flow LLC <ektaflowllc@Ektas-MacBook-Pro.local>
2022-07-26 09:43:51 -0500

    fix choose threads

--- src/app/Dashboard/New/ChooseThreads.tsx
+++ src/app/Dashboard/New/ChooseThreads.tsx
@@ -19,45 +19,41 @@ const ChooseThreads: React.FC<({ threads: any, setThreads: any })> = ({ threads,
       <div className="choose-threads-inner">
         <div className="left-side-picks">
           <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.t} onIonChange={e => setThreads({...threads, ['t']: !threads.t})} slot="start" color="dsb" />
-            <IonLabel>Technology Maturity</IonLabel>
-          </IonItem>
-          <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.a} slot="start" color="dsb" />
+            <IonCheckbox checked={threads.a} onIonChange={e => setThreads({...threads, ['a']: !threads.a})} slot="start" color="dsb" />
             <IonLabel>A. Technology & Industrial Base</IonLabel>
           </IonItem>
           <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.b} slot="start" color="dsb" />
+            <IonCheckbox checked={threads.b} onIonChange={e => setThreads({...threads, ['b']: !threads.b})} slot="start" color="dsb" />
             <IonLabel>B. Design</IonLabel>
           </IonItem>
           <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.c} slot="start" color="dsb" />
+            <IonCheckbox checked={threads.c} onIonChange={e => setThreads({...threads, ['c']: !threads.c})} slot="start" color="dsb" />
             <IonLabel>C. Cost & Funding</IonLabel>
           </IonItem>
           <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.d} slot="start" color="dsb" />
+            <IonCheckbox checked={threads.d} onIonChange={e => setThreads({...threads, ['d']: !threads.d})}  slot="start" color="dsb" />
             <IonLabel>D. Materials</IonLabel>
+          </IonItem>
+          <IonItem color="docentlight"   >
+            <IonCheckbox checked={threads.e} onIonChange={e => setThreads({...threads, ['e']: !threads.e})}  slot="start" color="dsb" />
+            <IonLabel>E. Process Capability & Control</IonLabel>
           </IonItem>
         </div>
         <div className="right-side-picks">
           <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.e} slot="start" color="dsb" />
-            <IonLabel>E. Process Capability & Control</IonLabel>
-          </IonItem>
-          <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.f} slot="start" color="dsb" />
+            <IonCheckbox checked={threads.f} onIonChange={e => setThreads({...threads, ['f']: !threads.f})}  slot="start" color="dsb" />
             <IonLabel>F. Quality Management</IonLabel>
           </IonItem>
           <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.g} slot="start" color="dsb" />
+            <IonCheckbox checked={threads.g} onIonChange={e => setThreads({...threads, ['g']: !threads.g})}  slot="start" color="dsb" />
             <IonLabel>G. Mfg Personnel</IonLabel>
           </IonItem>
           <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.h} slot="start" color="dsb" />
+            <IonCheckbox checked={threads.h} onIonChange={e => setThreads({...threads, ['h']: !threads.h})}  slot="start" color="dsb" />
             <IonLabel>H. Facilities</IonLabel>
           </IonItem>
           <IonItem color="docentlight"   >
-            <IonCheckbox checked={threads.i} slot="start" color="dsb" />
+            <IonCheckbox checked={threads.i} onIonChange={e => setThreads({...threads, ['i']: !threads.i})}  slot="start" color="dsb" />
             <IonLabel>I. Mfg Management</IonLabel>
           </IonItem>
         </div>
